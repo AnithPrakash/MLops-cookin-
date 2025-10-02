f2935b03e5582e4ddf9bd630e7b5b838b254e981
Anith Prakash <64299704+AnithPrakash@users.noreply.github.com>
2025-10-02 08:35:26 +0000

    break for 10

--- 03-Ochestration/prefect/aws/create_s3.py
+++ 03-Ochestration/prefect/aws/create_s3.py
@@ -3,7 +3,21 @@ from prefect_aws import S3Bucket, AwsCredentials
 
 def create_aws_cred_blocks():
     my_aws_creds_obj = AwsCredentials(
-        aws_access_key_id="1234",
-        aws_secret_access_key="PLACEHOLDER",
+        aws_access_key_id="",
+        aws_secret_access_key="",
         region_name="us-east-2"
     )
+    my_aws_creds_obj.save(name="my-aws-creds", overwrite=True)
+
+
+def create_s3_bucket_block():
+    aws_creds = AwsCredentials.load("my-aws-creds")
+    my_s3_buckets_obj=S3Bucket(
+        bucket_name="myawsbucket20345", credentials=aws_creds
+    )
+    my_s3_buckets_obj.save(name="s3-bucket-example", overwrite=True)
+
+if __name__=="__main__":
+    create_aws_cred_blocks()
+    sleep(5)
+    create_s3_bucket_block()

42bf2dd709a1bb9e7397922b6dcf193fc5a641c1
Anith Prakash <64299704+AnithPrakash@users.noreply.github.com>
2025-11-13 16:30:08 +0000

    trying

--- 06-Best_practices/integration-test/run.sh
+++ 06-Best_practices/integration-test/run.sh
@@ -5,6 +5,8 @@ cd "$(dirname "$0")"
 
 LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
 export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
+export PREDICTIONS_STREAM_NAME="ride_prediction"
+
 
 docker build -t ${LOCAL_IMAGE_NAME} .
 
@@ -12,14 +14,13 @@ docker-compose up -d
 
 sleep 1
 
-# docker run -it --rm \
-#     -p 8080:8080 \
-#     -e PREDICTIONS_STREAM_NAME="ride_predictions" \
-#     -e RUN_ID="Test123" \
-#     -e MODEL_LOCATION="/app/model/" \
-#     -e TEST_RUN="True" \
-#     -e AWS_DEFAULT_REGION="eu-west-1" \
-#     -v /workspaces/MLops-cookin-/06-Best_practices/integration-test/model:/app/model \
-#     stream-model-duration:v2
+aws --endpoint-url=http://localhost:4566 \
+    kinesis create-stream \
+          --stream-name ${PREDICTIONS_STREAM_NAME} \
+          --shard-count 1
 
 pipenv run python test_docker.py
+
+#docker-compose-down
+
+#exit ${ERROR_CODE}
